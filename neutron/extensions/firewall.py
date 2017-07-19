huntxu/neutron

commit d26e41e4c070e0dd687bd960aaae893959b62c5b

FWaaS: support some more protocols in FW rules
* GRE (47)
* ESP (50)
* AH (51)
* SCTP (132)

Fixes: redmine #10240

Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

neutron/extensions/firewall.py
@@ -151,7 +151,9 @@ class FirewallRuleConflict(qexception.Conflict):
                 "another tenant %(tenant_id)s")
 
 
-fw_valid_protocol_values = [None, constants.TCP, constants.UDP, constants.ICMP]
+fw_valid_protocol_values = [None, constants.TCP, constants.UDP, constants.ICMP
+                            constants.SCTP, constants.GRE,
+                            constants.ESP, constants.AH]
 fw_valid_action_values = [constants.FWAAS_ALLOW, constants.FWAAS_DENY]
 
 

neutron/plugins/common/constants.py
@@ -72,6 +72,10 @@ FWAAS_DENY = "deny"
 TCP = "tcp"
 UDP = "udp"
 ICMP = "icmp"
+SCTP = "sctp"
+GRE = "gre"
+ESP = "esp"
+AH = "ah"
 
 # Network Type constants
 TYPE_FLAT = 'flat'

neutron/services/firewall/drivers/linux/iptables_fwaas.py
@@ -311,7 +311,7 @@ class IptablesFwaasDriver(fwaas_base.FwaasDriverBase):
         return ''
 
     def _port_arg(self, direction, protocol, port):
-        if not (protocol in ['udp', 'tcp'] and port):
+        if not (protocol in ['udp', 'tcp', 'sctp'] and port):
             return ''
         return '--%s %s' % (direction, port)
 

commit 65a4fd2eb8c60e7466c0a996629097a616abfa7c

Fix syntax error
Fixes: d26e41e4c ("FWaaS: support some more protocols in FW rules")
Fixes: redmine #10240

Signed-off-by: Hunt Xu <mhuntxu@gmail.com>

neutron/extensions/firewall.py
@@ -151,7 +151,7 @@ class FirewallRuleConflict(qexception.Conflict):
                 "another tenant %(tenant_id)s")
 
 
-fw_valid_protocol_values = [None, constants.TCP, constants.UDP, constants.ICMP
+fw_valid_protocol_values = [None, constants.TCP, constants.UDP, constants.ICMP,
                             constants.SCTP, constants.GRE,
                             constants.ESP, constants.AH]
 fw_valid_action_values = [constants.FWAAS_ALLOW, constants.FWAAS_DENY]